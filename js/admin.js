ca9ad14313c8cf18d6cc9eb50406d67ffef7a791
RokasS <rokassd9@gmail.com>
2024-03-29 17:29:23 +0100

    final design

--- js/admin.js
+++ js/admin.js
@@ -38,6 +38,8 @@ const openModalSupprButtons = document.querySelectorAll('.openModalSuppr');
 const openModalAddButtons = document.querySelectorAll('.openModalEdit');
 const openModalInacButtons = document.querySelectorAll('.openModalInac');
 const openModalCreateButtons = document.querySelectorAll('.openModalCreate');
+const openModalAddCompte = document.querySelectorAll('.openModalAddCompte');
+const openModalEditCompte = document.querySelectorAll('.openModalEditCompte');
 const cancelButton = document.querySelectorAll('.cancelButton');
 
 const modal = document.getElementById('modal');
@@ -46,6 +48,28 @@ const fenetreAdd = document.querySelector('.page-ajouter');
 const fenetreInactif = document.querySelector('.page-inactif');
 const fenetreArgent = document.querySelector('.page-argent');
 const fenetreMonth = document.querySelector('.page-update');
+const fenetreCompte = document.querySelector('.page-compte');
+const fenetreEditCompte = document.querySelector('.page-edit-compte');
+
+openModalAddCompte.forEach(function(button) {
+  button.addEventListener('click', function() {
+    modal.style.display = 'flex';
+    fenetreCompte.style.display = 'flex';
+    var divInac = button.getAttribute('data-id');
+    var inputInac = document.getElementById("inputInac");
+    inputInac.setAttribute("value", divInac);
+  });
+});
+
+openModalEditCompte.forEach(function(button) {
+  button.addEventListener('click', function() {
+    modal.style.display = 'flex';
+    fenetreEditCompte.style.display = 'flex';
+    var divInac = button.getAttribute('data-id');
+    var inputInac = document.getElementById("inputInac");
+    inputInac.setAttribute("value", divInac);
+  });
+});
 
 openModalInacButtons.forEach(function(button) {
     button.addEventListener('click', function() {
@@ -97,6 +121,8 @@ openModalCreateButtons.forEach(function(button) {
     fenetreInactif.style.display = 'none';
     fenetreArgent.style.display = 'none';
     fenetreMonth.style.display = 'none';
+    fenetreCompte.style.display = 'none';
+    fenetreEditCompte.style.display = 'none';
     var divCancel = button.getAttribute('data-id');
     });
 });
@@ -109,6 +135,8 @@ modal.addEventListener('click', function(event) {
     fenetreInactif.style.display = 'none';
     fenetreArgent.style.display = 'none';
     fenetreMonth.style.display = 'none';
+    fenetreCompte.style.display = 'none';
+    fenetreEditCompte.style.display = 'none';
   }
 });
 
@@ -155,4 +183,184 @@ menu_links.forEach(function(link) {
             behavior: 'smooth'
         });
     });
+});
+
+//
+
+var map = L.map('map').setView([0, 0], 2); // Vue mondiale
+  
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+}).addTo(map);
+
+// Exemple de données sur les ventes
+var salesData = [
+  { location: [48.866, 0.333], intensity: 10 }
+  // Ajoute d'autres données ici
+];
+
+// Ajoute des marqueurs sur la carte pour chaque emplacement
+salesData.forEach(function(data) {
+  var marker = L.circleMarker(data.location, {
+    radius: data.intensity,
+    color: 'blue',
+    fillColor: 'blue',
+    fillOpacity: 0.5
+  }).addTo(map);
+});
+
+/*----------------------------------------------------*/
+
+const PourColor = [
+    '#02C667','#FF6F61','#F9C74F', '#F9C74F ', '#448AFF', '#FFD700', '#32CD32',
+    '#FF6347', '#8A2BE2', '#00FFFF', '#FFA500', '#808080',
+    '#FF1493', '#00CED1', '#FF4500', '#800080', '#00FF00',
+    '#FF00FF', '#008000', '#FFFF00', '#D2691E'
+];
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getMonthNames(startMonth) {
+    const months = [];
+    const date = new Date();
+    
+    for (let i = startMonth - 1; i < startMonth + 11; i++) {
+        date.setMonth(i % 12);
+        const monthName = new Intl.DateTimeFormat('fr-FR', { month: 'long' }).format(date);
+        months.push(capitalizeFirstLetter(monthName));
+    }
+    return months;
+}
+
+const startingMonth = 2; // Par exemple, pour commencer par octobre, utilisez 10 pour octobre
+
+const monthLabels = getMonthNames(startingMonth);
+
+const dataCourbe = {
+    labels: monthLabels,
+    datasets: [
+        {
+            label: 'Année',
+            data: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160],
+            borderColor: PourColor[0],
+            tension: 0.3,
+        },
+        {
+            label: 'Mois',
+            data: [60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170],
+            borderColor: PourColor[1],
+            tension: 0.3,
+        }
+        // Ajoutez d'autres enregistrements ici si nécessaire
+    ]
+};
+
+// const optionsCourbe = {
+//     responsive: false,
+//     plugins: {
+//         legend: {
+//             display: false
+//         }
+//     },
+//     scales: {
+//         y: {
+//             ticks: {
+//                 callback: function(value) {
+//                     return value + ' €';
+//                 },
+//                 stepSize: 20, // Définit l'intervalle entre chaque graduation
+//                 min: 0, // Définit la valeur minimale sur l'axe y
+//                 max: 600 // Définit la valeur maximale sur l'axe y
+//             }
+//         }
+//     }
+// };
+
+
+// const courbe = document.getElementById('myCourbe');
+
+// new Chart(courbe, {
+//     type: 'line',
+//     data: dataCourbe,
+//     options: optionsCourbe
+// });
+
+/*----------------------------------------------------*/
+
+const donut = document.getElementById('myDonut');
+const PourLabels = ['Healthy', 'Vegan', 'Accessoires']; // Exemple de noms de compte
+const PourData = [8980, 3972, 1691]; // Exemple de données de compte
+
+const dataDonut = {
+    labels: PourLabels,
+    datasets: [
+        {
+            label: 'Ventes € ',
+            data: PourData,
+            backgroundColor: PourColor,
+            borderWidth: 1,
+            tension: 0.2,
+        }
+    ]
+};
+
+const optionsDonut = {
+    responsive: true,
+    plugins: {
+        legend: {
+            display: false
+        }
+    },
+};
+
+new Chart(donut, {
+    type: 'doughnut',
+    data: dataDonut,
+    options: optionsDonut
+});
+
+/*----------------------------------------------------*/
+
+const trait = document.getElementById('myTrait');
+
+const dataTrait = {
+    labels: PourLabels,
+    datasets: [
+        {
+            label: 'Ventes €',
+            data: PourData,
+            backgroundColor: PourColor,
+            borderWidth: 1,
+            tension: 0.2,
+        }
+    ]
+};
+
+const optionsTrait = {
+    responsive: true,
+    scales: {
+        x: {
+            display: false, // Masque l'axe x
+        },
+        y: {
+            ticks: {
+                callback: function(value) {
+                    return value + ' €';
+                }
+            }
+        }
+    },
+    plugins: {
+        legend: {
+            display: false,
+        },
+    },
+};
+
+new Chart(trait, {
+    type: 'bar',
+    data: dataTrait,
+    options: optionsTrait,
 });
